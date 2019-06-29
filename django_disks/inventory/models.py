3ad3bbfb4c34b0f122a27d2a90c484b4b5597329
Ewan88 <baileyewan@gmail.com>
2019-06-29 12:32:42 +0100

    fixes gitignore and registers admin models

--- django_disks/inventory/models.py
+++ django_disks/inventory/models.py
@@ -6,5 +6,5 @@ class Artist(models.Model):
 class Album(models.Model):
     title = models.CharField(max_length=255)
     year = models.IntegerField()
-    stock = models.IntegerField()
+    stock_level = models.IntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
--- django_disks/inventory/tests.py
+++ django_disks/inventory/tests.py
@@ -5,7 +5,7 @@ class AlbumTestCase(TestCase):
     def setUp(self):
         self.artist1 = Artist.objects.create(name= "Bucks Fizz")
         
-        self.album1 = Album.objects.create(title = "Makin' Yo' Mind Up", year = 1989, stock = 10, artist = self.artist1)
+        self.album1 = Album.objects.create(title = "Makin' Yo' Mind Up", year = 1989, stock_level = 10, artist = self.artist1)
 
     def test_artist_saved(self):
         self.assertGreater(self.artist1.pk, 0)
