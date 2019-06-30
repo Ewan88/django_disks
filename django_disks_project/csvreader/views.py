28a8a3483c642a213d37bdeee87e3b094a15e7c0
Ewan88 <baileyewan@gmail.com>
2019-06-30 14:28:28 +0100

    tests csv creation

--- django_disks_project/csvreader/views.py
+++ django_disks_project/csvreader/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import csv
 
 def index(request):
-    return render(request, "csv/index.html", locals())
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="text/test.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Row 1', 'Column 1'])
+    writer.writerow(['Row 2', 'Column 2'])
+    return response
