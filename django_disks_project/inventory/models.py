511103d091f3383034a780c7e2f12b28a3e78719
Ewan88 <baileyewan@gmail.com>
2019-06-30 01:49:10 +0100

    fixes string names on models and home route

--- django_disks_project/inventory/models.py
+++ django_disks_project/inventory/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=255)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 class Album(models.Model):
@@ -12,5 +12,5 @@ class Album(models.Model):
     stock_level = models.IntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return self.title
