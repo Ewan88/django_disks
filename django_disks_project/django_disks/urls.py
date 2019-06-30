34e1dea74b9606696ad3c1dc9a52b0237c2888e5
Ewan88 <baileyewan@gmail.com>
2019-06-30 12:38:48 +0100

    adds csv app with urls and views

--- django_disks_project/django_disks/urls.py
+++ django_disks_project/django_disks/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('csv', include('csvreader.urls')),
     path('', include('inventory.urls')),
     path('admin/', admin.site.urls),
 ]
