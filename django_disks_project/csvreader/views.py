f9418b5297b8275d913a27bd620d288b77d4aaca
Ewan88 <baileyewan@gmail.com>
2019-06-30 19:14:13 +0100

    displays csv in html

--- django_disks_project/csvreader/views.py
+++ django_disks_project/csvreader/views.py
@@ -6,8 +6,8 @@ import os
 def index(request):
     csvdata = []
     with open('text/test.csv', mode='r', newline='') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in csvreader:
-            csvdata.append(', '.join(row))
+            csvdata.append(row)
             
     return render(request, 'csv/index.html', locals())
