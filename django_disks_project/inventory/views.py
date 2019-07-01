965d637cd98a5c2259a7fe05603c277bc477ba0c
Ewan88 <baileyewan@gmail.com>
2019-07-01 09:45:06 +0100

    adds working form saving to db but broken redirect

--- django_disks_project/inventory/views.py
+++ django_disks_project/inventory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from .forms import AlbumForm
 
@@ -12,8 +12,10 @@ def create(request):
     if request.method == 'POST':
         form = AlbumForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            return render(request, "inventory/index.html", locals())
+            data = form.cleaned_data
+            album = Album(title=data["title"], year=data["year"], stock_level=data["stock_level"], artist=data["artist"])
+            album.save()
+            return HttpResponseRedirect('')
     else:
         form = AlbumForm()
     
